99612bea9d59bfa2db5089e80c7fd2630e950b2e
Tamal saha <tamalsaha019@gmail.com>
2024-09-01 17:51:58 +0530

    CaseChecking

---
+++ CaseCheck.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CaseCheck {
+    public static void main(String[] args) {
+        System.out.print("please enter something: ");
+        Scanner input = new Scanner(System.in);
+        char ch = input.next().trim().charAt(0);
+        
+        //Case Checking
+        if(ch > 97 && ch <122 ){
+            System.out.println("this is lower case.");
+        } else if(ch > 65 && ch < 90){
+            System.out.println("this is upper case.");
+        }
+        else{
+            System.out.println("error: thois isn't any alphabet.");
+        }
+        input.close();
+    }
+}
