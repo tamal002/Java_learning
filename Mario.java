f35e645ecdaeefe06fd43755352a5aa87855e684
Tamal saha <tamalsaha019@gmail.com>
2024-09-08 17:56:20 +0530

    mario for cs50

---
+++ Mario.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Mario {
+    public static void main(String[] args) {
+        int i, j, k, l, n;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter the height: ");
+        n = input.nextInt();
+
+        for(i = 1; i <= n; i++){
+
+            for(j = 5; j > i; j--){
+                System.out.print(" ");
+            }
+
+            for(k = 1; k <= i; k++){
+                System.out.print("#");
+            }
+
+            System.out.print("  ");
+
+            for(l = 1; l <= i; l++){
+                System.out.print("#");
+            }
+
+            System.out.print("\n");
+        }
+        input.close();
+
+    }
+}
